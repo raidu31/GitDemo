516e0ab34a65506d88cbf6eae98e99c7ae03094f
Paparaidu Sanka <paparaidu.sanka@MAC41G.local>
2017-03-20 15:20:56 +0530

    new controller

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hi")
+        print("New hi")
+        print("New hi")
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
